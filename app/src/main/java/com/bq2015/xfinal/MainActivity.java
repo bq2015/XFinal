3897f5d0751df75a59e213348f475c4d29be58ee
bq2015 <Gin2358#>
2016-06-24 22:51:08 +0800

    建立依赖关系

--- app/src/main/java/com/bq2015/xfinal/MainActivity.java
+++ app/src/main/java/com/bq2015/xfinal/MainActivity.java
@@ -1,18 +1,39 @@
 package com.bq2015.xfinal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.bq2015.bqlib.mvvm.base.BaseActivity;
+import com.bq2015.xfinal.vm.MainActivityVM;
+
+public class MainActivity extends BaseActivity<MainActivity,MainActivityVM> {
+
+
+    private MainActivityVM mViewModel;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        init();
+    protected void getBundleExtras(@NonNull Bundle extras) {
+
     }
 
-    private void init() {
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        mViewModel = getViewModel();
+    }
 
+    @Override
+    protected int tellMeLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected View getStatusTargetView() {
+        return null;
+    }
+
+    @Override
+    public Class getViewModelClass() {
+        return null;
     }
 }
---
+++ app/src/main/java/com/bq2015/xfinal/vm/MainActivityVM.java
@@ -0,0 +1,26 @@
+package com.bq2015.xfinal.vm;
+
+import android.support.annotation.NonNull;
+
+import com.bq2015.bqlib.mvvm.AbstractViewModel;
+import com.bq2015.xfinal.MainActivity;
+
+/**
+ *
+ * Created by Kylin on 2016/6/24.
+ */
+public class MainActivityVM extends AbstractViewModel<MainActivity> {
+
+    private MainActivity mView;
+
+    @Override
+    public void onBindView(@NonNull MainActivity view) {
+        super.onBindView(view);
+        mView = getView();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+}
